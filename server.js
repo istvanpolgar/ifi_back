4cebd2e10c178986d90e3c92fd77989d420fb891
istvanpolgar <istvanpolgar@yahoo.com>
2021-08-30 12:37:30 +0300

    team

--- server.js
+++ server.js
@@ -22,61 +22,84 @@ const signupSchema = require('./src/schems/signupSchema');
 //PORT
 const port = process.env.PORT || 8080;
 
-app.post('/signin', async (req, res) => {
-    try{
-        const { email, password } = req.body;
-        await signinSchema.validateAsync(req.body)
-            .then( () => {
-                admin.signInWithEmailAndPassword(email, password)
-                    .then( () => { 
-                        if(!admin.currentUser.emailVerified){
-                            admin.signOut();
-                            res.send({code: 400, message: "A regisztráció nem került megerősítésre! Kérlek nézd meg a megadott e-mail fiókod!"});
-                        }
-                        else
-                            if(email == "ifiteka@gmail.com")
-                              res.send({
-                                role: "organizer"
-                              });
-                            else
-                              res.send({
-                                role: "team"
-                              });
-                    })
-            })
-            .catch((error) => {
-                res.send({code: 400, message: error.message});
-            })
-      } catch (e) {
-        res.send({code: 400, message: "Minden mezőt kötelező kitölteni!"});
-      }
-});
+//TOKEN
+const refreshTokenSecret = 'thisisatokensecret';
+let refreshTokens = [];
+const jwt = require('jsonwebtoken');
 
-app.post('/signup', async (req, res) => {
-    try{
-      const { team, email, password } = req.body;
-      await signupSchema.validateAsync(req.body)
-      .then( () => { 
-        admin.createUserWithEmailAndPassword(email, password)
-        .then( () => {
-          admin.currentUser.sendEmailVerification()
-          .then( () => {
-            database.ref('users/' + admin.currentUser.uid)
-            .set({
-                team: team,
-                email: email,
-                password: password
-            })
-            .then( () => {
-              res.send({status: 'Validated'})
+app.post('/signin', async (req, res) => {
+  const { email, password } = req.body;
+  await signinSchema.validateAsync(req.body)
+    .then( () => {
+        admin.signInWithEmailAndPassword(email, password)
+            .then( () => { 
+                if(!admin.currentUser.emailVerified){
+                    admin.signOut();
+                    res.send({code: 400, message: "A regisztráció nem került megerősítésre! Kérlek nézd meg a megadott e-mail fiókod!"});
+                }
+                else
+                {
+                  let accessToken = '';
+                  if(email == "ifiteka@gmail.com" && password == "legjobbcsapat") {
+                    accessToken = jwt.sign({ 
+                        id: admin.currentUser.uid,  
+                        role: 'organizer'
+                      }, 
+                      refreshTokenSecret,
+                      { expiresIn: '24h' }
+                    );
+                  } else {
+                    accessToken = jwt.sign({ 
+                        id: admin.currentUser.uid,  
+                        role: 'team'
+                      }, 
+                      refreshTokenSecret,
+                      { expiresIn: '1h' }
+                    );
+                  }
+                  refreshTokens.push(accessToken);
+
+                  res.send({
+                    token: accessToken,
+                    role: jwt.decode(accessToken).role
+                  });
+                }
             })
             .catch((error) => {
               res.send({code: 400, message: error.message});
             })
-          })
-          .catch((error) => {
-            res.send({code: 400, message: error.message});
-          })
+    })
+    .catch((error) => {
+        res.send({code: 400, message: error.message});
+    })
+});
+
+app.post('/signup', async (req, res) => {
+  const { team, email, password } = req.body;
+  await signupSchema.validateAsync(req.body)
+  .then( () => { 
+    admin.createUserWithEmailAndPassword(email, password)
+    .then( () => {
+      admin.currentUser.sendEmailVerification()
+      .then( () => {
+        database.ref('users/' + admin.currentUser.uid)
+        .set({
+            team: team,
+            email: email,
+            password: password,
+            xp: 0,
+            point: 0,
+            ores: {
+              iron: 0,
+              bronze: 0,
+              silver: 0,
+              gold: 0,
+              diamond: 0,
+              ifirald: 0
+            }
+        })
+        .then( () => {
+          res.send({status: 'Validated'})
         })
         .catch((error) => {
           res.send({code: 400, message: error.message});
@@ -85,10 +108,15 @@ app.post('/signup', async (req, res) => {
       .catch((error) => {
         res.send({code: 400, message: error.message});
       })
-    } catch (e) {
-      res.send({code: 400, message: "Minden mező kitöltése kötelező!"});
-    }
-  });
+    })
+    .catch((error) => {
+      res.send({code: 400, message: error.message});
+    })
+  })
+  .catch((error) => {
+    res.send({code: 400, message: error.message});
+  })
+});
 
 app.post('/forgotten_pass', async (req, res) => {
   try{
@@ -105,6 +133,162 @@ app.post('/forgotten_pass', async (req, res) => {
     res.send({code: 400, message: "Both fields are required!"});
   }
 })
+
+app.post('/team_info', async (req, res) => {
+  const token = req.headers.authorization.split(' ')[1];
+
+  if (!token) {
+      return res.send({code: 400, message: "Hiányzó token!"});;
+  }
+
+  if (!refreshTokens.includes(token)) {
+      return res.send({code: 400, message: "Helytelen token!"});
+  }
+
+  jwt.verify(token, refreshTokenSecret, async (err) => {
+    if (err) {
+        return res.send({code: 400, message: "Nem létező token!"});
+    }
+    await database.ref('users/' + jwt.decode(token).id)
+      .once('value')
+      .then((user) => {
+        res.send({
+          "token": token,
+          "role": jwt.decode(token).role,
+          "stats": user.val()
+        });
+      })
+      .catch((error) => {
+        res.send({code: 400, message: error.message});
+      })
+  });
+});
+
+app.post('/ore_prices', async (req, res) => {
+  const token = req.headers.authorization.split(' ')[1];
+
+  if (!token) {
+      return res.send({code: 400, message: "Hiányzó token!"});;
+  }
+
+  if (!refreshTokens.includes(token)) {
+      return res.send({code: 400, message: "Helytelen token!"});
+  }
+
+  jwt.verify(token, refreshTokenSecret, async (err) => {
+    if (err) {
+        return res.send({code: 400, message: "Nem létező token!"});
+    }
+    await database.ref('prices')
+      .once('value')
+      .then((prices) => {
+        res.json({
+          "token": token,
+          "role": jwt.decode(token).role,
+          "prices": prices.val()
+        });
+      })
+      .catch((error) => {
+        res.send({code: 400, message: error.message});
+      })
+  });
+});
+
+app.post('/shop', async (req, res) => {
+  const token = req.headers.authorization.split(' ')[1];
+  const shop = req.body;
+
+  if (!token) {
+      return res.send({code: 400, message: "Hiányzó token!"});;
+  }
+
+  if (!refreshTokens.includes(token)) {
+      return res.send({code: 400, message: "Helytelen token!"});
+  }
+
+  jwt.verify(token, refreshTokenSecret, async (err) => {
+    if (err) {
+        return res.send({code: 400, message: "Nem létező token!"});
+    }
+    await database.ref('users/' + jwt.decode(token).id)
+      .once('value')
+      .then(async (user) => {
+        let ores = user.val().ores;
+        await database.ref('users/' + jwt.decode(token).id)
+          .child('ores')
+          .update({
+            'bronze': parseInt(ores.bronze) + parseInt(shop.bronze),
+            'diamond': parseInt(ores.diamond) + parseInt(shop.diamond),
+            'gold': parseInt(ores.gold) + parseInt(shop.gold),
+            'ifirald': parseInt(ores.ifirald) + parseInt(shop.ifirald),
+            'iron': parseInt(ores.iron) + parseInt(shop.iron),
+            'silver': parseInt(ores.silver) + parseInt(shop.silver),
+          })
+          .catch((error) => {
+            res.send({code: 400, message: error.message});
+        })
+      })
+      .catch((error) => {
+        res.send({code: 400, message: error.message});
+    })
+    await database.ref('users/' + jwt.decode(token).id)
+      .once('value')
+      .then(async (user) => {
+        let xp = user.val().xp;
+        await database.ref('users/' + jwt.decode(token).id)
+          .update({ 'xp': xp - shop.price})
+          .catch((error) => {
+            res.send({code: 400, message: error.message});
+        })
+      })
+      .catch((error) => {
+        res.send({code: 400, message: error.message});
+    })  
+    res.send({status: "Finished"});    
+  });
+});
+
+app.post('/get_teams', async (req, res) => {
+  const token = req.headers.authorization.split(' ')[1];
+
+  if (!token) {
+      return res.send({code: 400, message: "Hiányzó token!"});;
+  }
+
+  if (!refreshTokens.includes(token)) {
+      return res.send({code: 400, message: "Helytelen token!"});
+  }
+
+  jwt.verify(token, refreshTokenSecret, async (err) => {
+    if (err) {
+        return res.send({code: 400, message: "Nem létező token!"});
+    }
+    let selectable_teams = [];
+    await database.ref('users')
+      .once('value')
+      .then((teams) => {
+        teams.forEach(t => {
+          selectable_teams.push(t.val().team)
+        });
+        res.json({
+          "token": token,
+          "role": jwt.decode(token).role,
+          "teams": selectable_teams
+        });
+      })
+      .catch((error) => {
+        res.send({code: 400, message: error.message});
+      })   
+  });
+});
+
+app.post('/logout', (req, res) => {
+  const token = req.headers.authorization.split(' ')[1];
+  
+  refreshTokens = refreshTokens.filter(t => t !== token);
+
+  res.send({status: "Logged out!"});
+});
 
 app.listen(port, () => {
     console.log(`Education app listening at http://localhost:${port}`);
--- src/configs/firebaseConfig.js
+++ src/configs/firebaseConfig.js
@@ -1,12 +1,12 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyDTTvmfWOOqRojxMnp-qpZnkLORpgBEbTA",
-  authDomain: "ifipengo.firebaseapp.com",
-  databaseURL: "https://ifipengo-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "ifipengo",
-  storageBucket: "ifipengo.appspot.com",
-  messagingSenderId: "339291678025",
-  appId: "1:339291678025:web:7440760e24577bc7e9e698",
-  measurementId: "G-K6S02X632Q"
+    apiKey: "AIzaSyDTTvmfWOOqRojxMnp-qpZnkLORpgBEbTA",
+    authDomain: "ifipengo.firebaseapp.com",
+    databaseURL: "https://ifipengo-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "ifipengo",
+    storageBucket: "ifipengo.appspot.com",
+    messagingSenderId: "339291678025",
+    appId: "1:339291678025:web:7440760e24577bc7e9e698",
+    measurementId: "G-K6S02X632Q"
 };
   
 module.exports = firebaseConfig;
